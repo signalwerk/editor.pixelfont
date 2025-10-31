02eae70a31ded4cdfe3ed6c296ee217df923f890
signalwerk <sh@signalwerk.ch>
2025-10-31 14:22:22 +0100

    MOD: change to vite

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import "./components/Input/styles.css";
 import "./components/Textarea/styles.css";
 
 import App from "./components/App/";
-import reportWebVitals from "./reportWebVitals";
 import { FontContextProvider } from "./components/Store/context";
 
 const root = ReactDOM.createRoot(
@@ -19,8 +18,3 @@ root.render(
     </FontContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

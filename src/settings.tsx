39df05b160ddb4f43a4f1e48ca5940b9ac91d7b6
signalwerk <sh@signalwerk.ch>
2025-10-31 15:53:18 +0100

    MOD: cleanup

--- src/settings.tsx
+++ src/settings.tsx
@@ -2,25 +2,6 @@ export const charMap = ["nomiueaskx", "cbdpqlhvwz", "ftgjyr.:@/", "0123456789"];
 export const chars = [...charMap, " "].join("").split("");
 
 export const settings = {
-  qrSize: 20,
-  page: {
-    width: 297,
-    height: 210,
-    padding: 18,
-    layout: {
-      columns: 10,
-      rows: 4,
-      gap: {
-        columns: 1,
-        rows: 3,
-      },
-    },
-  },
-  uid: "dos",
-  layout: {
-    version: 1,
-    type: "full-1",
-  },
   font: {
     descender: 1,
     xHeight: 5,
